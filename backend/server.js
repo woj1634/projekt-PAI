8a2807796cd71e08212cecd9cdac0dc114fdb691
woj1634 <woj1634@gmail.com>
2026-06-02 21:01:51 +0200

    konfiguracja azure 5

--- backend/server.js
+++ backend/server.js
@@ -48,4 +48,6 @@ app.put('/api/events/:id', async (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(5000, () => console.log('Backend na porcie 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Backend działa na porcie ${PORT}`));
